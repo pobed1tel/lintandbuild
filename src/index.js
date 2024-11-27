add538242be5eb69d6a9989196a3c47fda516e72
pobed1tel <s1mpleegamee@gmail.com>
2024-11-27 08:05:00 +0000

    change index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = require("express"):
+const express = require("express");
 
-const app = express = express();
+const app = (express = express());
 
-app.get("/", (req, res)=> {
-	res.json({
-		message: "Hello World",
-	});
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
 });
 
- app.listen(5050, () => console.log("Listening on http:/localhost:5050"));
+app.listen(5050, () => console.log("Listening on http:/localhost:5050"));
